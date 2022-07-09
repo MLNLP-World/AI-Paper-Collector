cc7fb18c6f2039b97a06d9f6ac14a6f2eb86237b
beiyuouo <beiyu233@gmail.com>
2022-07-09 14:38:19 +0800

    update cache

--- crawler.py
+++ crawler.py
@@ -16,65 +16,64 @@ def search_from_iclr(url, name, res):
     if name not in res:
         res[name] = []
     for item in data["notes"]:
-        res[name].append({
-                          "paper_name": item["content"]["title"],
-                          'paper_url': 'https://openreview.net'+item["content"]["pdf"]
-                          })
+        res[name].append(
+            {"paper_name": item["content"]["title"], "paper_url": "https://openreview.net" + item["content"]["pdf"]}
+        )
     return res
 
 
 def search_from_nips(url, name, res):
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, "html.parser")
     if name not in res:
         res[name] = []
-    for paper_item in soup.find(class_='col').ul.find_all('a'):
-        res[name].append({
-                          "paper_name": paper_item.string,
-                          'paper_url': "https://papers.nips.cc"+paper_item["href"]
-                          })
+    for paper_item in soup.find(class_="col").ul.find_all("a"):
+        res[name].append({"paper_name": paper_item.string, "paper_url": "https://papers.nips.cc" + paper_item["href"]})
     return res
 
 
 def search_from_acl(url, tag, name, res):
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, "html.parser")
     if name not in res:
         res[name] = []
-    for tp in soup.find_all('p',class_="d-sm-flex align-items-stretch"):
-        cls=tp.find('strong')
+    for tp in soup.find_all("p", class_="d-sm-flex align-items-stretch"):
+        cls = tp.find("strong")
         for paper_item in cls.find_all(href=re.compile(tag), class_="align-middle"):
             items = [item.string if item.string else item for item in paper_item.contents]
             try:
                 paper = "".join([item for item in items if isinstance(item, str)])
-                paper_url='https://aclanthology.org'+paper_item['href']
+                paper_url = "https://aclanthology.org" + paper_item["href"]
             except:
                 import pdb
+
                 pdb.set_trace()
-            res[name].append({"paper_name":paper,
-                              'paper_url': paper_url,
-                              })
+            res[name].append(
+                {
+                    "paper_name": paper,
+                    "paper_url": paper_url,
+                }
+            )
     return res
 
 
 def search_from_dblp(url, name, res):
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, "html.parser")
     if name not in res:
         res[name] = []
 
-    for paper_item in soup.find_all("li", class_='inproceedings'):
-        paper_url=paper_item.find("li", class_="drop-down").div.a['href']
-        paper_name=paper_item.find(class_='title', itemprop="name")
+    for paper_item in soup.find_all("li", class_="inproceedings"):
+        paper_url = paper_item.find("li", class_="drop-down").div.a["href"]
+        paper_name = paper_item.find(class_="title", itemprop="name")
         items = [item.string if item.string else item for item in paper_name.contents]
         try:
             paper = "".join([item for item in items if isinstance(item, str)])
         except:
             import pdb
+
             pdb.set_trace()
-        res[name].append({"paper_name":paper,
-                          'paper_url':paper_url
-                          })
+        res[name].append({"paper_name": paper, "paper_url": paper_url})
     return res
 
 
@@ -93,26 +92,26 @@ def crawl(cache_file=None):
         cache_res = json.load(open(cache_file, "r"))
         cache_conf = [name for name in cache_res.keys()]
 
-    # for conf in tqdm(acl_conf, desc="[+] Crawling ACL", dynamic_ncols=True):
-    #     assert conf.get("name") and conf.get("url") and conf.get("tag")
-    #     url, tag, name = conf["url"], conf["tag"], conf["name"]
-    #     if name in cache_conf:
-    #         continue
-    #     res = search_from_acl(url, tag, name, res)
-    #
-    # for conf in tqdm(dblp_conf, desc="[+] Crawling DBLP", dynamic_ncols=True):
-    #     assert conf.get("name") and conf.get("url")
-    #     url, name = conf["url"], conf["name"]
-    #     if name in cache_conf:
-    #         continue
-    #     res = search_from_dblp(url, name, res)
-    #
-    # for conf in tqdm(nips_conf, desc="[+] Crawling NeurIPS", dynamic_ncols=True):
-    #     assert conf.get("name") and conf.get("url")
-    #     url, name = conf["url"], conf["name"]
-    #     if name in cache_conf:
-    #         continue
-    #     res = search_from_nips(url, name, res)
+    for conf in tqdm(acl_conf, desc="[+] Crawling ACL", dynamic_ncols=True):
+        assert conf.get("name") and conf.get("url") and conf.get("tag")
+        url, tag, name = conf["url"], conf["tag"], conf["name"]
+        if name in cache_conf:
+            continue
+        res = search_from_acl(url, tag, name, res)
+
+    for conf in tqdm(dblp_conf, desc="[+] Crawling DBLP", dynamic_ncols=True):
+        assert conf.get("name") and conf.get("url")
+        url, name = conf["url"], conf["name"]
+        if name in cache_conf:
+            continue
+        res = search_from_dblp(url, name, res)
+
+    for conf in tqdm(nips_conf, desc="[+] Crawling NeurIPS", dynamic_ncols=True):
+        assert conf.get("name") and conf.get("url")
+        url, name = conf["url"], conf["name"]
+        if name in cache_conf:
+            continue
+        res = search_from_nips(url, name, res)
 
     for conf in tqdm(iclr_conf, desc="[+] Crawling ICLR", dynamic_ncols=True):
         assert conf.get("name") and conf.get("url")
--- update_cache.py
+++ update_cache.py
@@ -4,48 +4,63 @@ import ast
 import argparse
 from crawler import do_crawl
 
+
 def set_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--issue', '-i', type=str, help='input issue', required=True)
+    parser.add_argument("--issue", "-i", type=str, help="input issue", required=True)
     args = parser.parse_args()
     return args
 
+
 def parse_issue(issue):
     try:
-        issue = issue.replace('\\n', '').replace('\\r', '')
+        issue = issue.replace("\\n", "").replace("\\r", "")
         info = ast.literal_eval(issue)
+        print(info)
         assert isinstance(info, list)
-        assert len(info) > 0 and info[0].get('url') and info[0].get('name') and info[0].get('source')
+        assert len(info) > 0 and info[0].get("url") and info[0].get("name") and info[0].get("source")
     except:
-        raise Exception('[-] Wrong input!')
+        raise Exception("[-] Wrong input!")
     return info
 
+
+def check_duplicate(confs, item):
+    for conf in confs:
+        if conf["name"] == item["name"]:
+            return True
+    return False
+
+
 def add_list(args):
-    print('[+] Add list...')
+    print("[+] Add list...")
     info = parse_issue(args.issue)
     re_info = {}
     for item in info:
-        assert item.get('source') in ['nips', 'acl', 'dblp', 'iclr']
-        source = item.get('source')
+        assert item.get("source") in ["nips", "acl", "dblp", "iclr"]
+        source = item.get("source")
         if source not in re_info:
             re_info[source] = []
-        del item['source']
+        del item["source"]
         re_info[source].append(item)
 
     for source in re_info.keys():
-        path = os.path.join('conf', source + '_conf.json')
-        confs = json.load(open(path, 'r')) # [{'url': '', 'name': ''}]
+        path = os.path.join("conf", source + "_conf.json")
+        confs = json.load(open(path, "r"))  # [{'url': '', 'name': ''}]
         for item in re_info[source]:
             # need to mannally check duplicated conferences
+            if check_duplicate(confs, item):
+                print(f"[-] {item['name']} already exists!")
+                continue
             confs.append(item)
-        json.dump(confs, open(path, 'w'), indent=4)
-    print('[+] Add list Done!')
+        json.dump(confs, open(path, "w"), indent=4)
+    print("[+] Add list Done!")
+
 
 def main():
     args = set_args()
     add_list(args)
-    do_crawl(cache_file='cache/cache.json', force=True)
-    
-if __name__ == '__main__':
-    main()
+    do_crawl(cache_file="cache/cache.json", force=True)
 
+
+if __name__ == "__main__":
+    main()
