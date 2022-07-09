ac6519b68ffc2cc5cd3418c2c386026a65981adc
beiyuouo <beiyu233@gmail.com>
2022-07-09 13:51:25 +0800

    chore: ready to deploy

--- app.py
+++ app.py
@@ -160,5 +160,5 @@ def result():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=True)
-    # app.run(debug=False, host="0.0.0.0", port=5000, use_reloader=False)
+    # app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=True)
+    app.run(debug=False, host="0.0.0.0", port=5000, use_reloader=False)
