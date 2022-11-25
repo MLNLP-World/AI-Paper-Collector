0b0bf6e6fe83d3d554a2c36b99034d4daa39a2cf
Gordon Lee <guodun.li@gmail.com>
2022-11-26 00:44:48 +0800

    fix: update app.py

--- app.py
+++ app.py
@@ -230,4 +230,4 @@ def result():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000, use_reloader=True)
+    app.run(debug=True, host="0.0.0.0", port=80, use_reloader=True)
